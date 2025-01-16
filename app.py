6d5a6ebcab067a443883b82c07e91d38f1750b99
Oleh Synyshyn <oleg.synyshyn10@gmail.com>
2025-01-16 18:17:00 +0200

    16.01.25

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ db = DBManager("blog.db")
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
     categories = db.get_categories()
+    articles = db.get_articles()
     print(categories)
-    return render_template("index.html", categories=categories)  # html-сторінка, що повертається у браузер
+    return render_template("index.html", categories=categories, articles=articles)  # html-сторінка, що повертається у браузер
 
+@app.route("/category/<int:category_id>")  # Вказуємо url-адресу для виклику функції
+def category_page(category_id):
+    categories = db.get_categories()
+    articles = db.get_articles_by_category(category_id)
+    return render_template("index.html", categories=categories, articles=articles)  # html-сторінка, що повертається у браузер
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
--- db_scripts.py
+++ db_scripts.py
@@ -3,11 +3,29 @@ class DBManager():
     def __init__(self, dbname):
         self.dbname = dbname
         self.conn = None
-    
-    def get_categories(self):
+
+    def open_db(self):
         self.conn = sqlite3.connect(self.dbname)
         self.cursor = self.conn.cursor()
+       
+    
+    def get_categories(self):
+        self.open_db()
         self.cursor.execute('''SELECT * FROM categories''')
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+    
+    def get_articles(self):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles''')
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+
+    def get_articles_by_category(self, category_id):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles articles WHERE category_id=?''', [category_id])
         data = self.cursor.fetchall()
         self.conn.close()
         return data
